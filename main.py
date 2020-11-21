4f441353f3b20569dfba1425a0b3f2475dd39b2d
Maximilian von Kuick <maxvonkuick@outlook.de>
2020-11-21 18:26:48 +0100

    "First version of basic bot, runs and listens to channel messages and responds on activation keyword."

---
+++ main.py
@@ -0,0 +1,42 @@
+# IMPORT DISCORD.PY. ALLOWS ACCESS TO DISCORD'S API.
+import discord
+
+# Import the os module.
+import os
+# Import load_dotenv function from dotenv module.
+from dotenv import load_dotenv
+# Loads the .env file that resides on the same level as the script.
+load_dotenv()
+# Grab the API token from the .env file.
+DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
+
+# GETS THE CLIENT OBJECT FROM DISCORD.PY. CLIENT IS SYNONYMOUS WITH BOT.
+bot = discord.Client()
+
+# EVENT LISTENER FOR WHEN THE BOT HAS SWITCHED FROM OFFLINE TO ONLINE.
+@bot.event
+async def on_ready():
+	# CREATES A COUNTER TO KEEP TRACK OF HOW MANY GUILDS / SERVERS THE BOT IS CONNECTED TO.
+	guild_count = 0
+
+	# LOOPS THROUGH ALL THE GUILD / SERVERS THAT THE BOT IS ASSOCIATED WITH.
+	for guild in bot.guilds:
+		# PRINT THE SERVER'S ID AND NAME.
+		print(f"- {guild.id} (name: {guild.name})")
+
+		# INCREMENTS THE GUILD COUNTER.
+		guild_count = guild_count + 1
+
+	# PRINTS HOW MANY GUILDS / SERVERS THE BOT IS IN.
+	print("SampleDiscordBot is in " + str(guild_count) + " guilds.")
+
+# EVENT LISTENER FOR WHEN A NEW MESSAGE IS SENT TO A CHANNEL.
+@bot.event
+async def on_message(message):
+	# CHECKS IF THE MESSAGE THAT WAS SENT IS EQUAL TO "HELLO".
+	if message.content == "hello Sherlock":
+		# SENDS BACK A MESSAGE TO THE CHANNEL.
+		await message.channel.send("Hello there.")
+
+# EXECUTES THE BOT WITH THE SPECIFIED TOKEN. TOKEN HAS BEEN REMOVED AND USED JUST AS AN EXAMPLE.
+bot.run(DISCORD_TOKEN)
